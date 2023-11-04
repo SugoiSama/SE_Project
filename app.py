b2460202842cc844db35b8aa49d9b3e3955d9f39
SugoiSama <114637235+SugoiSama@users.noreply.github.com>
2023-11-04 11:23:28 +0000

    Commit before render

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask (__name__)
 
+PRODUCT = [
+  {
+    'id': 1,
+    'name': 'Pick up Service',
+    'description': 'Pick up service for pickup trucks and delivery trucks'	
+  },
+  {
+    'id': 2,
+    'name': 'Fertilizers',
+    'description': 'Fertilizers for plants'	
+  },
+  {
+    'id': 3,
+    'name': 'Earthworms',
+    'description': 'Earthworms for plants'
+  }
+]
+
 @app.route ('/')
 def hello_world ():
-  return render_template('home.html')
+  return render_template('home.html', products=PRODUCT)
+  
+@app.route("/api/products")
+def list_products():
+  return jsonify(PRODUCT)
 
 if __name__ == '__main__':
   app.run (host='0.0.0.0', debug = True)	
